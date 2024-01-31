ae8af294d4c742cf7ac3e0d9e33c5aaeab0d7bfa
Pat <harkle@gmx.net>
2024-01-31 20:55:26 +0100

    adding example deployments for RASPIS

--- install/installjenkins.sh
+++ install/installjenkins.sh
@@ -1,9 +1,11 @@
 apt update && apt upgrade
-apt install openjdk-17-jdk-headless git
+apt install openjdk-17-jdk-headless git sshpass ansible
 curl https://pkg.jenkins.io/debian/jenkins.io-2023.key | gpg --dearmor | tee /usr/share/keyrings/jenkins-archive-keyring.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/jenkins-archive-keyring.gpg] https://pkg.jenkins.io/debian binary/" >> /etc/apt/sources.list.d/jenkins.list
 apt update && apt install jenkins
 mkdir /home/pi/build
+chown jenkins. /home/pi/build
 mkdir /home/pi/deploy
+chown jenkins. /home/pi/deploy
 echo "goto http://<IP>:8080"
-echo "install plugins: git hub, ssh, ansible"
+echo "install plugins: ansible and stuff like gradle version...."
